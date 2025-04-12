2bdb5d477817f225860d46c3ceafa46a84103b44
rahulsah123 <smartrahulsah17@gmail.com>
2025-04-13 02:26:20 +0530

    Gemini ai chat

---
+++ src/main/java/com/gemini/ai/chat/gemini_ai_chat/GeminiAiChatApplication.java
@@ -0,0 +1,13 @@
+package com.gemini.ai.chat.gemini_ai_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiAiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiAiChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemini/ai/chat/gemini_ai_chat/GeminiAiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemini.ai.chat.gemini_ai_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiAiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
